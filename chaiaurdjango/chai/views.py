3da434d8889b538eacb396eeda8b30cbeac99b68
kumar saurabh <kumarsaurabh27d@gmail.com>
2024-07-08 10:22:47 +0530

    new commit

--- chaiaurdjango/chai/views.py
+++ chaiaurdjango/chai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import ChaiVarity
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarityForm
 # Create your views here.
 def all_chai(request):
     chais=ChaiVarity.objects.all()
@@ -9,3 +10,17 @@ def all_chai(request):
 def chai_detail(request,chai_id):
     chai=get_object_or_404(ChaiVarity,pk=chai_id)
     return render(request,'chai/chai_detail.html',{'chai':chai})
+
+def chai_store_view(request):
+    stores=None
+if request.method== 'POST':
+    form =ChaiVarityForm(request.POST)
+    if form.is_valid():
+    chai_variety=  form.cleaned_data['chai_variety']
+    store=Store.objects.filter
+    (chai_varieties=chai_variety)
+else:
+    form=ChaiVarityForm()
+    return render(request,'chai/chai_stores.html',{'stores':stores,'form':form})
+
+
